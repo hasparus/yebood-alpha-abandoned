ccb2462d26b5da2b956a411ce08a2d74c609feaa
hasparus <hasparus@gmail.com>
2016-11-03 16:24:12 +0000

    Modify .env.

--- config/application.rb
+++ config/application.rb
@@ -6,9 +6,14 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+Dotenv::Railtie.load
+
+
 module Yebood
   class Application < Rails::Application
-    config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')    # Settings in config/environments/* take precedence over those specified here.
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
+
+    # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
