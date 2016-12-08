76ad26c5d61e3aa5e64d8fe32ad91eec72e3f7a6
Michal Mordawski <michal@mordawski.it>
2016-12-08 17:58:54 +0100

    User tests started.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,5 +2,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @krystian = build :user
+  end
+
+
+  it 'has a valid factory' do
+    expect(@krystian.name).equal? 'Krystian'
+    #todo more
+  end
+
+  it 'should not create user without email' do
+    cnt = User.count
+    @krystian.email = ''
+    @krystian.save
+    expect(User.count).equal? cnt
+  end
+
+  it 'test user#name' do
+    @krystian.name = ''
+    @krystian.save!
+    expect(1).to equal(2)
+  end
 end
