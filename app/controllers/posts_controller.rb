5f83e6ffe02ff02f1284f5db5b122d9b6ad5caee
hasparus <hasparus@gmail.com>
2017-01-25 10:01:29 +0100

    .

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,20 @@
 # frozen_string_literal: true
 class PostsController < ApplicationController
+  before_action :set_category
+  before_action :set_topic
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   respond_to :html, :json, :js
 
   def new
-    @post = Post.new
+    @post = @topic.posts.new user: current_user
   end
 
   def edit
   end
 
   def create
-    @post = Post.new(post_params)
+    @post = @topic.posts.new(post_params)
     @post.save
     respond_with(@post)
   end
@@ -32,6 +34,14 @@ class PostsController < ApplicationController
 
   def set_post
     @post = Post.find(params[:id])
+  end
+
+  def set_category
+    @category = Category.find_by category_slug: params[:category_category_slug]
+  end
+
+  def set_topic
+    @topic = Topic.find_by topic_slug: params[:topic_topic_slug]
   end
 
   def post_params
